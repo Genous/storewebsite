28cc8b05c221df1fa6e2c32f7125500cc2bcbe8f
knarjp <kevajess@gmail.com>
2016-12-05 21:35:39 -0500

    Add files via upload

--- employee.php
+++ employee.php
@@ -7,15 +7,28 @@
 		                      $password = 'd4taBas3r', // password - dont change
 		                      $connection_string = '//oracle.cise.ufl.edu/orcl'); // database URL - dont change
 	$x = $_SESSION['userId'];
-
 	$statement = oci_parse($connection, 'SELECT * FROM EMPLOYEE WHERE EMPLOYEEID = :x'); // SQL query
 	oci_bind_by_name($statement, ":x", $x);
 	oci_execute($statement); // execute the query
 	echo "Welcome, ";
 	while (($row = oci_fetch_array($statement, OCI_BOTH)) != false)
 	{
-		    echo $row[1]. " ".$row[2]."<br>";
+		    echo $row[1]." ".$row[2]."<br>";
 	}
+
+//        oci_free_statement($statement); // dont change
+//        oci_close($connection); // dont change
+
+	$statement = oci_parse($connection, 'SELECT IT.itemID, IT.itemType, IT.itemSize, IT.wholeSaleCost FROM ITEM IT, EMPLOYEE EMP WHERE EMP.EMPLOYEEID = :x AND EMP.DEPTARTMENT = IT.DEPARTMENT');
+	oci_bind_by_name($statement, ':x', $x);
+	oci_execute($statement);
+	
+	echo "you sell: ";
+
+        while (($row = oci_fetch_array($statement, OCI_BOTH)) != false)
+        {
+                    echo $row[0]." ".$row[1]." ".$row[2]." ".$row[3]."<br>";
+        }
 
 	oci_free_statement($statement); // dont change
 	oci_close($connection); // dont change
